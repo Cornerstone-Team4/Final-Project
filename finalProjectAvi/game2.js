b01c68f7b3c88d2ce07eb3cce805d6319264cebe
Avinoam Ruach <ruachavinoam@gmail.com>
2019-09-30 11:41:40 -0700

    game2

---
+++ finalProjectAvi/game2.js
@@ -0,0 +1,21 @@
+function checkPrime(inputNumber){
+    var flag = true;
+    if (inputNumber == 0){
+        flag = false;
+    }
+    if (inputNumber == 1){
+        flag = true;
+    }
+    for(var i = 2; i < inputNumber; i++) {
+        if(inputNumber % i === 0) {
+            flag = false;
+        }
+        
+    }if (flag == true){
+            document.write("The number " + inputNumber + " is a prime number.");
+        }if (flag == false){
+            document.write("The number " + inputNumber + " is not a prime number.");
+        }
+    return flag;
+    
+}
